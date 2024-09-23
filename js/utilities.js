ac20aa93a2cdc946dfab18ab0b3b50339e6b5899
huzaifa-abir <huzaifaabir8@gmail.com>
2024-09-23 23:32:46 +0600

    blog page add

--- js/utilities.js
+++ js/utilities.js
@@ -17,11 +17,3 @@ function showSection(id){
     document.getElementById('history').classList.add('hidden');
     document.getElementById(id).classList.remove('hidden');
 }
-
-function blog(){
-    window.location.href = '/index.html';
-}
-function home(){
-    window.location.href = '/blog.html';
-    
-}
